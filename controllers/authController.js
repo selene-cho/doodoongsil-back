b955fa3bf0f2a6a1710a502fbd7fe2a5d63a9865
selene-cho <selene.cho29@gmail.com>
2023-12-08 16:13:02 +0900

    feat: kakao 로그인, 로그아웃

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,49 @@
 const User = require('../models/users');
-const ERRORS = require('../error/errorMessages');
 const { createAccessToken } = require('../middlewares/auth');
+const ERRORS = require('../error/errorMessages');
+const HttpError = require('../error/httpError');
+const { default: axios } = require('axios');
 
-exports.login = async (req, res, next) => {
-  const { kakaoId, nickname } = req.body;
-  console.log('req.body', req.body);
+const COOKIE_EXPIRATION_MS = 7 * 24 * 60 * 60 * 1000;
+
+exports.kakaoLogin = async (req, res, next) => {
+  const { code } = req.body;
 
   try {
+    const tokenResponse = await axios.post(
+      'https://kauth.kakao.com/oauth/token',
+      {},
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        params: {
+          grant_type: 'authorization_code',
+          client_id: process.env.KAKAO_REST_API_KEY,
+          redirect_uri: process.env.KAKAO_REDIRECT_URI,
+          code,
+        },
+      },
+    );
+
+    const { access_token: kakaoAccessToken } = tokenResponse.data;
+
+    const userInfoResponse = await axios.get(
+      'https://kapi.kakao.com/v2/user/me',
+      {
+        headers: {
+          Authorization: `Bearer ${kakaoAccessToken}`,
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      },
+    );
+
+    const {
+      id: kakaoId,
+      properties: { nickname },
+    } = userInfoResponse.data;
+
     let user = await User.findOne({ kakaoId });
-    console.log('user', user);
 
     if (!user) {
       user = await User.create({ kakaoId, nickname });
@@ -16,15 +51,45 @@ exports.login = async (req, res, next) => {
 
     const accessToken = createAccessToken(user);
 
-    res.status(200).json({ result: 'ok', accessToken });
-  } catch (err) {
-    if (err.name === 'ValidationError') {
-      const validationErrors = Object.values(err.errors).map(
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      path: '/',
+      sameSite: 'strict',
+      maxAge: COOKIE_EXPIRATION_MS,
+    });
+
+    res.status(200).json({ result: 'ok' });
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      const validationErrors = Object.values(error.errors).map(
         (err) => err.message,
       );
 
       return next(new HttpError(400, validationErrors));
     }
+    return next(new HttpError(500, ERRORS.PROCESS_ERR));
+  }
+};
+
+exports.logout = async (req, res, next) => {
+  const { kakaoId } = req.body;
+
+  try {
+    await axios.post(
+      'https://kapi.kakao.com/v1/user/logout',
+      { target_id_type: 'user_id', target_id: kakaoId },
+      {
+        headers: {
+          Authorization: `KakaoAK ${process.env.KAKAO_ADMIN_KEY}`,
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      },
+    );
+
+    res.clearCookie('accessToken', { path: '/' });
+    res.status(200).json({ result: 'ok' });
+  } catch (error) {
     return next(new HttpError(500, ERRORS.PROCESS_ERR));
   }
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { login } = require('../controllers/authController');
+const { kakaoLogin, logout } = require('../controllers/authController');
 
-router.post('/login', login);
+router.post('/login/kakao', kakaoLogin);
+router.post('/logout', logout);
 
 module.exports = router;
